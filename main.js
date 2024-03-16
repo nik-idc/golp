cd5a440bae105d155d380b9684c4f9cd27663a9b
atikin <nikvmain@gmail.com>
2024-03-16 19:09:18 +0300

    Stylistic changes

--- main.js
+++ main.js
@@ -8,26 +8,38 @@ const stepForwardButton = document.getElementById("stepForwardButton");
 const stepBackwardButton = document.getElementById("stepBackwardButton");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
-sizeInput.value = 30;
+sizeInput.value = 25;
 
 // Create workers
 const leftId = "left";
+const middleId = "middle";
 const rightId = "right";
 const leftWorker = new Worker("./src/worker.js");
+const middleWorker = new Worker("./src/worker.js");
 const rightWorker = new Worker("./src/worker.js");
 leftWorker.postMessage(["setId", leftId]);
+middleWorker.postMessage(["setId", middleId]);
 rightWorker.postMessage(["setId", rightId]);
 let grid1 = null;
 let grid2 = null;
+let grid3 = null;
+
+// Min and max grid sizes
+const minSize = 4;
+const maxSize = 75;
 
 /**
  * Removes tables
  */
 const removePrevTables = () => {
   const leftTable = document.getElementById(leftId);
+  const middleTable = document.getElementById(middleId);
   const rightTable = document.getElementById(rightId);
   if (leftTable) {
     gamesContainer.removeChild(leftTable);
+  }
+  if (middleTable) {
+    gamesContainer.removeChild(middleTable);
   }
   if (rightTable) {
     gamesContainer.removeChild(rightTable);
@@ -41,7 +53,8 @@ const makeGrids = () => {
   removePrevTables();
 
   grid1 = new Grid(sizeInput.value, gamesContainer, leftId, leftWorker);
-  grid2 = new Grid(sizeInput.value, gamesContainer, rightId, rightWorker);
+  grid2 = new Grid(sizeInput.value, gamesContainer, middleId, middleWorker);
+  grid3 = new Grid(sizeInput.value, gamesContainer, rightId, rightWorker);
 
   stepBackwardButton.disabled = true;
 };
@@ -58,8 +71,13 @@ const main = () => {
       return;
     }
 
-    if (Number(sizeInput.value) <= 4) {
+    if (Math.floor(Number(sizeInput.value)) <= minSize) {
       alert("Input size too small");
+      return;
+    }
+
+    if (Math.floor(Number(sizeInput.value)) >= maxSize) {
+      alert("Input size too big");
       return;
     }
 
@@ -69,6 +87,7 @@ const main = () => {
   stepForwardButton.addEventListener("click", () => {
     grid1.stepForwardGame();
     grid2.stepForwardGame();
+    grid3.stepForwardGame();
 
     stepBackwardButton.disabled = false;
   });
@@ -76,6 +95,7 @@ const main = () => {
   stepBackwardButton.addEventListener("click", () => {
     grid1.stepBackwardGame();
     grid2.stepBackwardGame();
+    grid3.stepBackwardGame();
 
     stepBackwardButton.disabled = true;
   });
@@ -83,6 +103,7 @@ const main = () => {
   startButton.addEventListener("click", () => {
     grid1.startGame();
     grid2.startGame();
+    grid3.startGame();
 
     genButton.disabled = true;
     stepForwardButton.disabled = true;
@@ -93,6 +114,7 @@ const main = () => {
   stopButton.addEventListener("click", () => {
     grid1.stopGame();
     grid2.stopGame();
+    grid3.stopGame();
 
     genButton.disabled = false;
     stepForwardButton.disabled = false;
